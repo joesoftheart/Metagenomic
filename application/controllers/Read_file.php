ccbfeed173c3c74f1b343322927b533d6ebd0380
Joesoftheart <tonkha23@gmail.com>
2017-11-10 14:13:39 +0700

    read file page4 to report

--- application/controllers/Read_file.php
+++ application/controllers/Read_file.php
@@ -90,6 +90,11 @@ class Read_file extends CI_Controller{
         echo "Min shanon :" . min($arr_shanon)."<br>";
         echo "Max shanon :" . max($arr_shanon)."<br>";
         echo ">>>>>>>>>>>>>>>>>>>>>End Page 1-2<<<<<<<<<<<<<<<<<br>";
+
+
+
+
+
         $rare_index = array();
         $name_sample = array();
         $save_value = array();
@@ -160,73 +165,236 @@ class Read_file extends CI_Controller{
         echo '<br>';
         echo $name_sample[3].$save_value[3];
         echo '<br>';
-        echo ">>>>>>>>>>>>>>>>>>>>>End Page 3<<<<<<<<<<<<<<<<<br>";
+
 
 
         $name_sample_phylumn = array();
-        $value_phylumn =array();
+        $value_phylumn =[];
+        $save_value_phylumn = array();
+        $total_num = null;
+
         $phylumn_count = file_get_contents($path_owncloud_phylotype."file_phylum_count.txt");
         $row = explode("\n", $phylumn_count);
         $check_index = preg_split('/\s+/', $row[0]);
         array_shift($row);
-
+        $half_sam = count($check_index)/2;
         for($i=0;$i<count($check_index);$i++){
            $name_sample_phylumn[$i] = $check_index[$i];
+
         }
 
+//
+            $index = 0;
         foreach ($row as $value => $data){
             $row_data_phy = preg_split('/\s+/', $data);
 
             if ($data != null) {
+                $total_num = null;
+                for ($i = 0; $i < count($name_sample_phylumn); $i++) {
+                             $value_phylumn[$index][$i] = $row_data_phy[$i] . '<br>';
+                    $total_num += $row_data_phy[$i];
+                }
+                $save_value_phylumn[$index] =  $total_num;
+                // echo '<br>';
+            }
+            $index++;
+        }
 
-                for($j=0;$j<count($name_sample_phylumn);$j++){
+$max = 0;
+$k = 0;
+$more = 0;
+        foreach ($save_value_phylumn as $key => $value) {
+            if ($value >= $max) {
+                $max = max($save_value_phylumn);
+                $k = $key;
+            }
+        }
+
+       for ($n = 0;$n<count($name_sample_phylumn);$n++){
+           echo $value_phylumn[$k][$n];
+           if ($value_phylumn[$k][$n] > $more){
+               $more = $value_phylumn[$k][$n];
+           }
+       }
 
 
-                    
+        for ($n = 0;$n<count($name_sample_phylumn);$n++){
+            echo $name_sample_phylumn[$n];
+        }
+        echo "Big SAM :".$more;
+        echo '<br>';
+
+
+
+
+
+
+        echo ">>>>>>>>>>>>>>>>>>>>>End Page 3<<<<<<<<<<<<<<<<<br>";
+
+        $genus = file_get_contents($path_owncloud_phylotype."file_after_reverse.csv");
+        $row = explode("\n", $genus);
+        array_shift($row);
+        $count_genus = array();
+        $num = 0;
+        $key_index = null;
+
+        foreach ($row as $value => $data) {
+            echo $value.'<br>';
+            if ($data != null ) {
+                $split = preg_split('/,/', $data);
+
+
+                if ($count_genus == null) {
+                    for ($j = 0; $j < count($split); $j++) {
+                        $count_genus[$j] =  $split[$j];
+                        if ($num < $split[$j]){
+                            $num = $split[$j];
+                            $key_index = $j;
+                        }
+                    }
+                }else{
+                    for ($j = 1; $j < count($split); $j++) {
+                        $count_genus[$j] +=  $split[$j];
+                        if ($num < $split[$j]){
+                            $num = $split[$j];
+                            $key_index = $j;
+                        }
+                    }
+
                 }
 
 
 
             }
+
+        }
+
+        echo $key_index;
+        echo $num;
+        $k_genus = 0;
+        $max_genus = 0;
+        foreach ($count_genus as $key => $value) {
+            if ($value >= $max_genus) {
+                $max_genus = max($count_genus);
+                $k_genus = $key;
+            }
+        }
+        echo $max_genus;
+
+        $row_default = explode("\n", $genus);
+        $split_row = preg_split('/,/', $row_default[0]);
+        echo $split_row[$key_index];
+        echo $num * 100 / $count_genus[$key_index];
+
+
+
+
+
+
+
+        echo ">>>>>>>>>>>>>>>>>>>>>End Page 4 <<<<<<<<<<<<<<<<<br>";
+        $axes1 = array();
+        $axes2 = array();
+        $near_data = file_get_contents($path_owncloud_phylotype."final.tx.thetayc.2.lt.ave.nmds.axes");
+        $row = explode("\n", $near_data);
+        array_shift($row);
+        $index = 0;
+        foreach ($row as $value => $data) {
+
+            if ($data != null) {
+                $split = preg_split('/\s+/', $data);
+
+              $axes1[$index] = $split[1];
+               $axes2[$index] = $split[2];
+             $index++;
+            }
+        }
+
+//        $save_near_count = 0 ;
+//        for ($i = 0; $i < count($axes1); $i++) {
+//
+//            for ($j = 0; $j < count($axes1); $j++) {
+//
+//                    if ($i != $j) {
+//                        if ($save_near_count == null) {
+//                             $save_near_count =  abs($axes1[$i] - $axes1[$j]) . '<br>';
+//                        }else{
+//
+//                            if ($save_near_count > abs($axes1[$i] - $axes1[$j])) {
+//                                $save_near_count = abs($axes1[$i] - $axes1[$j]);
+//                                echo $i . $j;
+//                                echo '<br>';
+//                            }else{
+//
+//                            }
+//                        }
+//                    }
+//            }
+//            echo "<br>";
+//        }
+//
+        $sam1 = null;
+        $sam2 = null;
+        $save_near_county = 0 ;
+        for ($i = 0; $i < count($axes2); $i++) {
+
+            for ($j = 0; $j < count($axes2); $j++) {
+
+                if ($i != $j) {
+                    if ($save_near_county == null) {
+                        $save_near_county =  sqrt(pow((($axes1[$i]) - ($axes1[$j])),2) + pow( (($axes2[$i]) - ($axes2[$j])),2));
+//                        echo $axes1[$i] - $axes1[$j];
+//                        echo '<br>';
+//                        echo $axes2[$i] - $axes2[$j];
+//                        echo '<br>';
+//                        echo $i . $j;
+//                        echo '<br>';
+//                        echo $save_near_county;
+                    }else{
+                        if ( $save_near_county > sqrt(pow((($axes1[$i]) - ($axes1[$j])),2) + pow((($axes2[$i]) - ($axes2[$j])),2 ))){
+                                $save_near_county =  sqrt(pow((($axes1[$i]) - ($axes1[$j])),2) + pow((($axes2[$i]) - ($axes2[$j])),2));
+//                            echo ($axes1[$i]) - ($axes1[$j]);
+//                            echo '<br>';
+//                            echo $axes2[$i] - $axes2[$j];
+//                            echo '<br>';
+//                            $sam1 =  $i ;
+//                            $sam2 = $j;
+//                            echo '<br>';
+//                            echo $save_near_county;
+                        }else{
+
+                        }
+                    }
+                }
+            }
+
+        }
+
+        $index = 0;
+        foreach ($row as $value => $data) {
+
+            if ($data != null) {
+                $split = preg_split('/\s+/', $data);
+                if ($index == $sam1){
+                    echo "Sam1". $split[0];
+                    echo '<br>';
+                }
+                if ($index == $sam2){
+                    echo "Sam2". $split[0];
+                    echo '<br>';
+                }
+                $index++;
+            }
         }
 
 
 
+        echo ">>>>>>>>>>>>>>>>>>>>>End Page 5 <<<<<<<<<<<<<<<<<br>";
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        echo "In db";
+        echo ">>>>>>>>>>>>>>>>>>>>>End Page 6 <<<<<<<<<<<<<<<<<br>";
+        echo "In db";
+        echo ">>>>>>>>>>>>>>>>>>>>>End Page 7 <<<<<<<<<<<<<<<<<br>";
 
         foreach ($path_file_phylotype as $value){
             if (file_exists($path_owncloud_phylotype . $value)) {
