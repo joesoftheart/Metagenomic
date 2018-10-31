9f78d84bb423ad1414070ca00e72fcff7a6a3382
root <root@aum.local>
2018-10-31 17:57:30 +0700

    push-code-mothur-qiime1-V1

--- STAMP-1.8/qsubStamp.sh
+++
@@ -1,2 +0,0 @@
-
-python  STAMP-1.8/commandLine.py --file $1 --sample1 $2 --sample2 $3 --statTest "G-test (w/ Yates' correction)"  --CI "DP: Newcombe-Wilson" -p 0.05 --coverage 0.95 --outputTable $4 
